079fd0f844c3aa9ffcd012849d15785e226a012c
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2025-11-20 15:28:57 -0300

    refactor: ajustar comentário sobre a URL base da API

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-// Configure a URL base da sua API aqui
+// URL BASE da API
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080"
 
 export const api = axios.create({
